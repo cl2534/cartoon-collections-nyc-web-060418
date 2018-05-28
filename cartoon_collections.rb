ef79007114205462eb4779b1aef0adc33176c143
Changmin Lim <chang.lim92@gmail.com>
2018-05-28 22:27:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,10 @@ def long_planeteer_calls(array)
   end 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.each do |i|
+    
+  
 end
